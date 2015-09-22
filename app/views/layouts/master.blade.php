87ebc63f8f270b9f2dac6fc324e5380b0f4e82df
Remington <remingtonwilliams@gmail.com>
2015-09-22 12:15:56 -0500

    added jquery cdn

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -61,6 +61,7 @@
     
 </main>
 <footer>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
