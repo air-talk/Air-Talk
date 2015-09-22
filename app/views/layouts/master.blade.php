3d76c003d8fb022a59a57f8554224cf0c6f25249
Remington <remingtonwilliams@gmail.com>
2015-09-22 12:44:18 -0500

    more master

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -22,7 +22,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Brand</a>
+          <a class="navbar-brand" href="#"><span class="black">AIR</span><span class="blue">Talk</span></a>
         </div>
     
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -54,10 +54,18 @@
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container -->
     </nav><!-- /.navbar -->
-    
-    
 </header>
 <main>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="well"></div>
+            </div>
+            <div class="col-md-4">
+                <div class="well"></div>
+            </div>
+        </div>
+    </div>
     
 </main>
 <footer>
