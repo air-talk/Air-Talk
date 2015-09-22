2e441865603f0952fdf9c5438622c1c2a71b7f97
asteph <alissa.stephens1@gmail.com>
2015-09-22 12:12:11 -0500

    changed faker text to real text

--- app/database/seeds/QuestionsTableSeeder.php
+++ app/database/seeds/QuestionsTableSeeder.php
@@ -9,11 +9,11 @@ class QuestionsTableSeeder extends Seeder {
 		for($i=0; $i<20; $i++)
 		{
 			$question = new Question();
-			$question->question = $faker->text(200);
-			$question->right_answer = $faker->text(10);
-			$question->wrong_answer1 = $faker->text(10);
-			$question->wrong_answer2 = $faker->text(10);
-			$question->wrong_answer3 = $faker->text(10);
+			$question->question = $faker->realText(200);
+			$question->right_answer = $faker->realText(10);
+			$question->wrong_answer1 = $faker->realText(10);
+			$question->wrong_answer2 = $faker->realText(10);
+			$question->wrong_answer3 = $faker->realText(10);
 			$question->category = 'untowered';
 			$question->save();	
 		}    
