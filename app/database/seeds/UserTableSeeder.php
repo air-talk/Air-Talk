17fa64f11a527f51416f37c0f6cd7fa99902ef7a
zakkyzebra <zzaacchhyy010@yahoo.com>
2015-09-22 10:00:24 -0500

    took out DOB

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,7 +6,6 @@ class UsersTableSeeder extends Seeder {
     {
         User::create([
 
-            'username'   => $_ENV['USER_USERNAME'],
             'first_name' => $_ENV['USER_FIRSTNAME'],
             'last_name'  => $_ENV['USER_LASTNAME'],
             'email'      => $_ENV['USER_EMAIL'],
