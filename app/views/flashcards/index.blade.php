94be25d9a3188a29f4f9178400714a8b87e57972
asteph <alissa.stephens1@gmail.com>
2015-09-22 16:04:03 -0500

    added modal to flashcards view

--- app/views/flashcards/index.blade.php
+++ app/views/flashcards/index.blade.php
@@ -2,6 +2,22 @@
 @section('head')
 @stop
 @section('content')
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                    <button type="button" class="close" data-dismiss="modal"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span></button>
+                    <br>
+                {{ Form::open(array('action' => 'FlashcardsController@index')) }}
+                    <div class="modal-body">
+                        <h2>{{$flashcards[0]->front}}</h2>
+                    </div>
+                {{-- </form> --}}
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
 	<div class="container">
 		<div class="row">
 		    <div class="col-md-offset-1 col-md-6">
@@ -29,11 +45,11 @@
 		    </div>
 		    <div class="col-md-4">
 		        <div class="well">
-					<button type="button" class="btn btn-primary btn-circle" aria-label="Left Align">
-					  Practice Flashcards
-					</button>
+		        	<!-- Trigger the login modal with a button -->
+                    <a type="button" class="btn btn-primary btn-circle" data-toggle="modal" data-target="#myModal">Practice Flashcards</a>
 		        </div>
 		    </div>
 		</div>
+		    <!-- Modal -->
 	</div>
 @stop
