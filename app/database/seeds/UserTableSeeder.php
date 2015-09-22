f05f1e2bfb160c64f69b7d5249e0f5be196500a7
asteph <alissa.stephens1@gmail.com>
2015-09-22 10:01:43 -0500

    added generators in composer.json; deleted dob from users table

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,7 +6,6 @@ class UsersTableSeeder extends Seeder {
     {
         User::create([
 
-            'dob'   => $_ENV['USER_DOB'],
             'first_name' => $_ENV['USER_FIRSTNAME'],
             'last_name'  => $_ENV['USER_LASTNAME'],
             'email'      => $_ENV['USER_EMAIL'],
