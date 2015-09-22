54e137dd1da717a60892036d54a947bb7aec1ba1
asteph <alissa.stephens1@gmail.com>
2015-09-22 11:51:32 -0500

    made changes to Question model and QuestionsTableSeeder

--- app/models/Question.php
+++ app/models/Question.php
@@ -6,9 +6,7 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Model implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
+class Question extends Model {
 
 	/**
 	 * The database table used by the model.
@@ -18,7 +16,7 @@ class User extends Model implements UserInterface, RemindableInterface {
 	protected $table = 'questions';
 
 
-	protected $fillable = ['question', 'right_answer', 'wrong_answer1', 'wrong_answer2', 'wrong_answer3', 'categories'];
+	protected $fillable = ['question', 'right_answer', 'wrong_answer1', 'wrong_answer2', 'wrong_answer3', 'category'];
 
 
 	/**
@@ -34,7 +32,7 @@ class User extends Model implements UserInterface, RemindableInterface {
 		'wrong_answer1' => 'required',
 		'wrong_answer2' => 'required',
 		'wrong_answer3' => 'required',
-		'categories' => 'required'
+		'category' => 'required'
 	);
 
 }
