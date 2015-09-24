516788c68d5cb34a7b7e9dd5b5c0eb6e5f02047b
asteph <alissa.stephens1@gmail.com>
2015-09-24 12:17:15 -0500

    changed flashcards seeder to include some examples for planes TODO: work on planes view so that it will only grab the flashcards from this category and display them

--- app/database/seeds/FlashcardsTableSeeder.php
+++ app/database/seeds/FlashcardsTableSeeder.php
@@ -4,11 +4,35 @@ class FlashcardsTableSeeder extends Seeder {
 
     public function run()
     {
+        $flashcard = new Flashcard();
+        $flashcard->front = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Cessna172-CatalinaTakeOff.JPG/300px-Cessna172-CatalinaTakeOff.JPG';
+        $flashcard->back = 'Cessna 172 Skyhawk';
+        $flashcard->category = 'plane';
+        $flashcard->save();
+
+        $flashcard = new Flashcard();
+        $flashcard->front = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/PiperPA-28-236DakotaC-GGFSPhoto4.JPG/300px-PiperPA-28-236DakotaC-GGFSPhoto4.JPG';
+        $flashcard->back = 'Piper PA-28 Cherokee';
+        $flashcard->category = 'plane';
+        $flashcard->save();
+
+        $flashcard = new Flashcard();
+        $flashcard->front = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Cessna.fa150k.g-aycf.arp.jpg/300px-Cessna.fa150k.g-aycf.arp.jpg';
+        $flashcard->back = 'Cessna 150';
+        $flashcard->category = 'plane';
+        $flashcard->save();
+
+        $flashcard = new Flashcard();
+        $flashcard->front = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Cessna182t_skylane_n2231f_cotswoldairshow_2010_arp.jpg/300px-Cessna182t_skylane_n2231f_cotswoldairshow_2010_arp.jpg';
+        $flashcard->back = 'Cessna 182 Skylane';
+        $flashcard->category = 'plane';
+        $flashcard->save();
+
         for($i = 0; $i < 15; $i++){
             $flashcard = new Flashcard();
             $flashcard->front = 'Front';
             $flashcard->back = 'Back';
-            $flashcard->category = 'Category';
+            $flashcard->category = 'vocab';
             $flashcard->save();
         }
     }
