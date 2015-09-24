9fa86a1a506b28bcceb1837017699685aa9c4638
asteph <alissa.stephens1@gmail.com>
2015-09-24 10:26:05 -0500

    redirected user create, set up mail template for implementation if desired (need to change and gitignore config files), changed sigin and create, added links

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -3,9 +3,14 @@
 <title>Sign Up</title>
 @stop
 @section('content')
-<div class="well" id="form">
-    {{ Form::open(array('action' => 'UsersController@store')) }}
-        @include('users.partials.create_user_form')
-    {{Form::close()}}
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 well col-md-offset-3">
+			<h3 class="thin text-center">Create a free account</h3>
+		    {{ Form::open(array('action' => 'UsersController@store')) }}
+		        @include('users.partials.create_user_form')
+		    {{Form::close()}}
+		</div>
+	</div>
 </div>
 @stop
