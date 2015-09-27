2180ee737f0ef579d70d073120c4975f02d7e529
asteph <alissa.stephens1@gmail.com>
2015-09-27 15:43:17 -0500

    added more content to welcome page and adjusted image sizing

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -103,19 +103,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2>Created By</h2>
+                    <h2>About</h2>
                     <hr class="star-light">
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-4">
-                    <p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
-                </div>
-                <div class="col-sm-4">
-                    <p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
-                </div>
-                <div class="col-sm-4">
-                    <p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
+                <div class="col-lg-8 col-lg-offset-2">
+                    Airtalk 
                 </div>
             </div>
         </div>
@@ -126,51 +120,22 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2>Contact Me</h2>
+                    <h2>Created by</h2>
                     <hr class="star-primary">
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
-                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
-                    <form name="sentMessage" id="contactForm" novalidate>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Name</label>
-                                <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Email Address</label>
-                                <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Phone Number</label>
-                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Message</label>
-                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <br>
-                        <div id="success"></div>
-                        <div class="row">
-                            <div class="form-group col-xs-12">
-                                <button type="submit" class="btn btn-success btn-lg">Send</button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="row text-center">
+                <div class="col-sm-4">
+                    <img src="/images/Alissa.jpg" class="img-circle img-responsive" alt="Alissa's headshot" style="margin: 0 auto" width="50%" height="440">
+                    <h3>Alissa Stephens</h3>
+                </div>
+                <div class="col-sm-4">
+                    <img src="http://lorempixel.com/500/500/people" class="img-circle img-responsive" alt="Zach's headshot" style="margin: 0 auto" width="50%" height="440">
+                    <h3>Zachary Hughes</h3>
+                </div>
+                <div class="col-sm-4">
+                    <img src="/images/Remington.jpg" class="img-circle img-responsive" alt="Remington's headshot" style="margin: 0 auto" width="50%" height="440">
+                    <h3>Remington Williams</h3>
                 </div>
             </div>
         </div>
@@ -206,8 +171,8 @@
                         </ul>
                     </div>
                     <div class="footer-col col-md-4">
-                        <h3>About Freelancer</h3>
-                        <p>Freelance is a free to use, open source Bootstrap theme.</p>
+                        <h3>Contact</h3>
+                        <p>airtalk@gmail.com</p>
                     </div>
                 </div>
             </div>
