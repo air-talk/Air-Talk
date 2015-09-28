b4b50313285c192e7b57e5fe05de3cc90ff8f7ee
asteph <alissa.stephens1@gmail.com>
2015-09-28 11:00:38 -0500

    created view that will come up with the questions for the user that is logged in

---
+++ app/views/questions/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+@section('head')
+@stop
+@section('content')
+	<div class="row">
+		<div class="col-md-8">
+		</div>
+	</div>
+	<div class="col-md-4">
+	    <div class="well">
+    	</div>
+    </div>	
+@stop
+@section('script')
+@stop
