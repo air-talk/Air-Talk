5197be5b475533e19e1f416a840dfb1b3b9c8310
asteph <alissa.stephens1@gmail.com>
2015-09-29 14:39:44 -0500

    downloaded plane images so that they loaded faster on the flashcards

--- app/database/seeds/FlashcardsTableSeeder.php
+++ app/database/seeds/FlashcardsTableSeeder.php
@@ -5,25 +5,25 @@ class FlashcardsTableSeeder extends Seeder {
     public function run()
     {
         $flashcard = new Flashcard();
-        $flashcard->front = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Cessna172-CatalinaTakeOff.JPG/300px-Cessna172-CatalinaTakeOff.JPG';
+        $flashcard->front = '/images/cessna172.png';
         $flashcard->back = 'Cessna 172 Skyhawk';
         $flashcard->category = 'plane';
         $flashcard->save();
 
         $flashcard = new Flashcard();
-        $flashcard->front = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/PiperPA-28-236DakotaC-GGFSPhoto4.JPG/300px-PiperPA-28-236DakotaC-GGFSPhoto4.JPG';
+        $flashcard->front = '/images/cherokee.png';
         $flashcard->back = 'Piper PA-28 Cherokee';
         $flashcard->category = 'plane';
         $flashcard->save();
 
         $flashcard = new Flashcard();
-        $flashcard->front = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Cessna.fa150k.g-aycf.arp.jpg/300px-Cessna.fa150k.g-aycf.arp.jpg';
+        $flashcard->front = '/images/cessna150.png';
         $flashcard->back = 'Cessna 150';
         $flashcard->category = 'plane';
         $flashcard->save();
 
         $flashcard = new Flashcard();
-        $flashcard->front = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Cessna182t_skylane_n2231f_cotswoldairshow_2010_arp.jpg/300px-Cessna182t_skylane_n2231f_cotswoldairshow_2010_arp.jpg';
+        $flashcard->front = '/images/cessna182.png';
         $flashcard->back = 'Cessna 182 Skylane';
         $flashcard->category = 'plane';
         $flashcard->save();
