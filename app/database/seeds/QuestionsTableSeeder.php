24bff3ffd21597b1a52a94d456dadb8002cf4a77
asteph <alissa.stephens1@gmail.com>
2015-10-01 10:53:04 -0500

    added vocab content to the flashcards seeder

--- app/database/seeds/QuestionsTableSeeder.php
+++ app/database/seeds/QuestionsTableSeeder.php
@@ -6,6 +6,54 @@ class QuestionsTableSeeder extends Seeder {
     {
         $faker = Faker::create();
 
+        $question = new Question();
+        $question->question = 
+        	"";
+        $question->right_answer = $faker->realText(10);
+        $question->wrong_answer1 = $faker->realText(10);
+        $question->wrong_answer2 = $faker->realText(10);
+        $question->wrong_answer3 = $faker->realText(10);
+        $question->category = 'nontowered';
+        $question->save();
+
+        $question = new Question();
+        $question->question = 
+        	"";
+        $question->right_answer = $faker->realText(10);
+        $question->wrong_answer1 = $faker->realText(10);
+        $question->wrong_answer2 = $faker->realText(10);
+        $question->wrong_answer3 = $faker->realText(10);
+        $question->category = 'nontowered';
+        $question->save();
+
+        $question = new Question();
+        $question->question = 
+        	"";
+        $question->right_answer = $faker->realText(10);
+        $question->wrong_answer1 = $faker->realText(10);
+        $question->wrong_answer2 = $faker->realText(10);
+        $question->wrong_answer3 = $faker->realText(10);
+        $question->category = 'nontowered';
+        $question->save();
+
+        $question = new Question();
+        $question->question = $faker->realText(200);
+        $question->right_answer = $faker->realText(10);
+        $question->wrong_answer1 = $faker->realText(10);
+        $question->wrong_answer2 = $faker->realText(10);
+        $question->wrong_answer3 = $faker->realText(10);
+        $question->category = 'nontowered';
+        $question->save();
+
+        $question = new Question();
+        $question->question = $faker->realText(200);
+        $question->right_answer = $faker->realText(10);
+        $question->wrong_answer1 = $faker->realText(10);
+        $question->wrong_answer2 = $faker->realText(10);
+        $question->wrong_answer3 = $faker->realText(10);
+        $question->category = 'nontowered';
+        $question->save();
+
 		for($i=0; $i<20; $i++)
 		{
 			$question = new Question();
@@ -14,7 +62,7 @@ class QuestionsTableSeeder extends Seeder {
 			$question->wrong_answer1 = $faker->realText(10);
 			$question->wrong_answer2 = $faker->realText(10);
 			$question->wrong_answer3 = $faker->realText(10);
-			$question->category = 'untowered';
+			$question->category = 'nontowered';
 			$question->save();	
 		}    
 	}
