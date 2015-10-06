9138c5c6d0f4b9f0fd4523f4c7cadef6cad34177
asteph <alissa.stephens1@gmail.com>
2015-10-06 09:55:04 -0500

    umm

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -11,20 +11,6 @@ class UsersTableSeeder extends Seeder {
             'password'   => $_ENV['USER_PASSWORD'],
             'password_confirmation'   => $_ENV['USER_PASSWORD'],
         ]);
-        User::create([
-            'first_name' => $_ENV['USER_FIRSTNAME2'],
-            'last_name'  => $_ENV['USER_LASTNAME2'],
-            'email'      => $_ENV['USER_EMAIL2'],
-            'password'   => $_ENV['USER_PASSWORD2'],
-            'password_confirmation'   => $_ENV['USER_PASSWORD2'],
-        ]);
-        User::create([
-            'first_name' => $_ENV['USER_FIRSTNAME3'],
-            'last_name'  => $_ENV['USER_LASTNAME3'],
-            'email'      => $_ENV['USER_EMAIL3'],
-            'password'   => $_ENV['USER_PASSWORD3'],
-            'password_confirmation'   => $_ENV['USER_PASSWORD3'],
-        ]);
     }
 
 }
